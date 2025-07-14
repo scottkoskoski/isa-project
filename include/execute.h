48aae8888f3c2aa37a6019928b7dd4832f4c9aeb
Scott Koskoski <scottkoskoski@gmail.com>
2025-07-14 18:12:23 -0500

    Added function definitions to execute.h

--- include/execute.h
+++ include/execute.h
@@ -5,5 +5,8 @@
 
 int32_t perform_alu_operation(AluOp operation, int32_t left, int32_t right);
 void execute_instruction(Instruction *inst, int32_t *result);
+int should_branch(Instruction *inst, int32_t alu_result);
+void update_program_counter(VirtualMachine *vm, Instruction *inst, int32_t alu_result);
+void execute_stage(VirtualMachine *vm, Instruction *inst, int32_t *result);
 
 #endif // EXECUTE_H
